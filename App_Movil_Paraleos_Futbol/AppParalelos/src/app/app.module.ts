391ce1f63fe4cea06bf98ccd267f76d1c7aa47d1
vpyojanny <102862361+vpyojanny@users.noreply.github.com>
2024-10-13 09:50:06 -0400

    Final

--- App_Movil_Paraleos_Futbol/AppParalelos/src/app/app.module.ts
+++ App_Movil_Paraleos_Futbol/AppParalelos/src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
-
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireMessagingModule } from '@angular/fire/messaging';
-import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule,AngularFireModule.initializeApp(environment.firebase),AngularFireMessagingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
